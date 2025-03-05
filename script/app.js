6271c50c4175c3c55c597e377c94e3c0f975c8bf
Wenard Santos <wenardken57@gmail.com>
2025-03-05 12:34:34 +0800

    refactor: Make grid from 4x4 to 16x16
    I made a mistake and made a 4x4 instead of 16x16 grid.

--- script/app.js
+++ script/app.js
@@ -6,9 +6,11 @@ gridSquare.setAttribute("class", "gridSquare");
 
 const squares = [];
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 256; i++) {
   squares.push(gridSquare.cloneNode(true));
 }
+
+squares[16].style.backgroundColor = 'red';
 
 squares.forEach((square) => {
   container.appendChild(square);
