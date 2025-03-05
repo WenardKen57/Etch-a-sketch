9c82fb3c7fe32bede2be9c7835a285a65c63db83
Wenard Santos <wenardken57@gmail.com>
2025-03-05 12:37:30 +0800

    Mark the 16th square of grid
    I made this so it's easier to organize the grid to 16x16

--- script/app.js
+++ script/app.js
@@ -10,7 +10,7 @@ for (let i = 1; i <= 256; i++) {
   squares.push(gridSquare.cloneNode(true));
 }
 
-squares[16].style.backgroundColor = 'red';
+squares[15].style.backgroundColor = 'red';
 
 squares.forEach((square) => {
   container.appendChild(square);
